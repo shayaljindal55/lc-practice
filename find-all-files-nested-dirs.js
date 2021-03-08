a59bf33753870f9b5efc0c0ead4773fa9b36a8d5
shayal.jindal <shayal.jindal@accenture.com>
2021-03-08 14:46:13 +0800

    Find all files in a nested directory

---
+++ find-all-files-nested-dirs.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+var path = require("path");
+var walk = function (dir, done) {
+  var results = [];
+  fs.readdir(dir, function (err, list) {
+    if (err) return done(err);
+    var i = 0;
+    (function next() {
+      var file = list[i++];
+      if (!file) return done(null, results);
+      file = path.resolve(dir, file);
+      fs.stat(file, function (err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function (err, res) {
+            results = results.concat(res);
+            next();
+          });
+        } else {
+          results.push(file);
+          next();
+        }
+      });
+    })();
+  });
+};
+const pathToDir = '/path/to/your/dir/';
+walk(pathToDir, function (err, results) {
+  if (err) throw err;
+  console.log(results);
+});
